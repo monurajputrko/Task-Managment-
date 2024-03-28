760bf5718e172d81dc710e89b8c7781ec9eaab20
Monu Singh <125896049+monurajputrko@users.noreply.github.com>
2024-03-28 13:02:40 +0530

    Update action.js

--- Frontend/src/Redux/AuthReducer/action.js
+++ Frontend/src/Redux/AuthReducer/action.js
@@ -49,6 +49,21 @@ export const signup = (data) => {
       );
       // console.log(res);
       dispatch({ type: SIGNUP_SUCCESS, paylod: res });
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "success",
+        title: "Account Created successfully",
+      });
       // console.log(res);
     } catch (error) {
       dispatch({ type: SIGNUP_FAILURE });
